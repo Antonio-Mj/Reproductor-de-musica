a25d911a49bfe314dbfb6c21f4bcbad931cf037e
Antonio-Mj <antonio.devpisc@gmail.com>
2024-08-04 17:59:39 -0600

    Implementación de Splash Screen

---
+++ app/src/main/java/com/example/reproductor/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.reproductor;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView BeatsTextView = findViewById(R.id.BeatsTextView);
+        ImageView LogoImageView = findViewById(R.id.LogoImageView);
+
+        BeatsTextView.setAnimation(animacion2);
+        LogoImageView.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
